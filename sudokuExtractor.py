ab1b7bd6a762121d116c0a22f0bd310c444bc238
Aymeric <aymeric.lavedrine@etu.univ-amu.fr>
2020-04-27 23:28:01 +0200

    update

--- sudokuExtractor.py
+++ sudokuExtractor.py
@@ -1,3 +1,10 @@
 import numpy as np
 import cv2
-import matplotlib
+
+print(cv2.__version__)
+
+# image = cv2.imread("sudoku.jpg")
+
+# cv2.imshow('Sudoku', image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
